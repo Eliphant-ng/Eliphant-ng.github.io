6457ae9544c230c9b7ab83d324a9a40175f0c70c
Eliphant Ng <s10204922@connect.np.edu.sg>
2020-12-22 12:29:30 +0800

    idk

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+/* header */
 $(document).ready(function(){
     $("#sidebarCollapse").on('click',function(){
         $("#sidebar").toggleClass('active')
     });
 });
+
+// geolocation // 
+$('#location-button').click(function(){
+
+    if(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(function(position){
+            console.log(position);
+        });
+    else
+        console.log("geolocation is not supported");
+});
+
